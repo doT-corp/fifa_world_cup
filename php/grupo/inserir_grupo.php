27bad39bf661f460833aa809642263d77c779e59
Guilherme Samuel <guisamuel800@gmail.com>
2021-10-18 16:35:09 -0300

    Formulários e PHP - Tabela Grupo

---
+++ php/grupo/inserir_grupo.php
@@ -0,0 +1,10 @@
+<?php
+    include "conecta_banco.php";
+    $insere = "INSERT INTO grupo 
+    (idgrupo, descrição) VALUES
+    ('$_POST[id]', '$_POST[descricao]')";
+
+    mysqli_query($conexao, $insere) or die("Não inseriu!");
+    echo "Dados inseridos no banco com sucesso!";
+    echo '<br><a href="index.html"><button>Voltar</button></a>'
+?>
