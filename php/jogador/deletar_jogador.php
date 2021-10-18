283567217e5891496fac13d2f76466d606de55ee
Diegusto <diegusto35@gmail.com>
2021-10-18 16:41:56 -0300

    alteração e exclusão de jogador

---
+++ php/jogador/deletar_jogador.php
@@ -0,0 +1,10 @@
+<?php
+    include "conecta_banco.php";
+    $id = $_POST['id'];
+    $query = "DELETE FROM jogador WHERE idjogador = '$id';";
+    if(mysqli_query($conexao, $query))
+        echo "Registro excluído com sucesso!";
+    else
+        echo "Falha na exclusão!";
+    echo '<br><a href="index.html"><button>Voltar</button></a>'
+?>
