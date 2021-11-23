b579597b4f32e1ddbecf3bc44d1548608bdde7d7
Guilherme Samuel <guisamuel800@gmail.com>
2021-11-23 16:35:54 -0300

    Update deletar_usuario.php

--- php/usuario/deletar_usuario.php
+++ php/usuario/deletar_usuario.php
@@ -229,7 +229,7 @@ button{
     }
     else
         echo "Falha na exclusão!";
-    echo '<br><a href="../../bottons-grupos.html"><button>Voltar</button></a>';
+    echo '<br><a href="../../bottons-usuarios.html"><button>Voltar</button></a>';
 ?>
 </body>
 </html>
