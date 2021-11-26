d302389d2a3c3c04e4e5a497c13a781720386491
Guilherme Samuel <guisamuel800@gmail.com>
2021-11-26 14:36:01 -0300

    Update listar_grupo.php

--- php/grupo/listar_grupo.php
+++ php/grupo/listar_grupo.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Listar Estádios</title>
+        <title>Listar Grupos</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="icon" href="../../assets/fifa_icon.png">
